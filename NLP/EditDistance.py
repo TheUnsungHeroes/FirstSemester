7fa81da3e06315bbbd989433ebc3e344f3460d9a
gotticketsto360tour <werling1407@gmail.com>
2020-09-10 11:55:29 +0200

    Start of dynamic programming

--- NLP/EditDistance.py
+++ NLP/EditDistance.py
@@ -1,24 +1,34 @@
 import numpy as np
 
+# ITERATIVE SOLUTION:
 
-def sub_cost(x, y, mode=2):
+
+def sub_cost(x, y, mode="leven"):
     if x == y:
         return 0
     else:
-        return mode
+        if mode == "leven":
+            return 2
+        else:
+            return 1
 
 
 def edit_distance(source, target, del_cost=1, ins_cost=1):
     n = len(source)
     m = len(target)
     distance_matrix = np.zeros((n+1, m+1))
+
     for row in range(1, n+1):
+
         distance_matrix[row, 0] = distance_matrix[row-1, 0] + del_cost
+
     for column in range(1, m+1):
+
         distance_matrix[0, column] = distance_matrix[0, column-1] + ins_cost
-    
+
     for row in range(1, n+1):
         for column in range(1, m+1):
+
             distance_matrix[row, column] = min(
                 distance_matrix[row-1, column] + del_cost,
                 distance_matrix[row-1, column-1] +
@@ -28,6 +38,8 @@ def edit_distance(source, target, del_cost=1, ins_cost=1):
     return distance_matrix
 
 
-print(edit_distance("intention", "execution"))
+print(edit_distance("drive", "brief"))
 
-    
+# DYNAMIC PROGRAMMING SOLUTION:
+
+some_df = edit_distance("drive", "brief")
