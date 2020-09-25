f8150661f0130554bc419249082b799dc8bb1094
gotticketsto360tour <werling1407@gmail.com>
2020-09-25 08:50:43 +0200

    seperate again...

--- NLP/class 4/text_processor_functions.py
+++ NLP/class 4/text_processor_functions.py
@@ -202,57 +202,7 @@ def lemma_pos_stanza(tokenlist):
 
     return [(sentence_number+1, word.id, word.text, word.lemma, word.pos) for sentence_number, sentence in enumerate(doc.sentences) for word in sentence.words]
     
-    
-class Text():
-    '''
-    This class contains the entire pipeline of pre-processing
-    of text using the text_processor_functions and stanza.
-    '''
-    def __init__(self, txt):
 
-        '''Initializing the class. This includes:
-        1. sentences = the sentences of the text in a list.
-        2. tokens = the tokens of the sentences in a list of lists. '''
-
-        self.sentences = sentence_segment(txt)
-        self.tokens = tokenize(self.sentences)
-
-    def ner(self, method="regex"):
-
-        '''
-        Returns a list of lists containing the tokens
-        of the named entities in the text.
-        '''
-
-        res = list(map(ner_regex, self.sentences))
-        names = [name for name in res if name]
-        names = tokenize(names[0])[0]
-
-        return names
-
-    def get_df(self):
-        """
-        returns a dataframe containing the columns:
-        sentence number, token, lemma, pos-tag, named-entity
-        """
-        tokens = self.tokens
-
-        names = test_sentence.ner()
-
-        names_list = []
-        for sentence in tokens:
-            for word in sentence:
-                names_list.append(word in names)
-
-        df = pd.DataFrame(lemma_pos_stanza(tokens), columns = ["Sent_No", "Word_No","Word", "Lemma", "POS"])
-        df["NE"] = names_list
-        return df
-
-    def get_ngrams(self, n):
-        """
-        returns n-grams on text
-        """
-        return [n_grams(token, n) for token in self.tokens]
 
 
 
